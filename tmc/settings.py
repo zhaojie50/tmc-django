b567744999f35c630b1bd28bfec9faab72e7d7a6
zhaoj <zhaojie50@163.com>
2022-08-10 00:00:13 +0800

    sqlite to mysql

--- tmc/settings.py
+++ tmc/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'tmc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tmc-django',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
